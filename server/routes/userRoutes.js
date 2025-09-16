636c03b695b117ccec0f4bee6d2a7266f750fcba
harrywardy-cmd <harrywardy303@gmail.com>
2025-09-16 22:46:17 +1000

    removing files

--- server/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import {create} from "../server/controller/controller/userController.js"
-
-const route = express.Router();
-
-route.post("/user",create)
-
-export default route;
