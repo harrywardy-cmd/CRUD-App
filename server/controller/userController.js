9bb4d7df3630c130bac3cf96103469c08718c203
harrywardy-cmd <harrywardy303@gmail.com>
2025-09-18 00:18:15 +1000

    create update user by ID method

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -43,3 +43,19 @@ export const getUserByID = async(req, res) =>{
     res.status(500).jason({errorMessage: error.message});
   }
 };
+
+export const update = async(req, res) =>{
+  try{
+    const id = req.params.id;
+    const userExist = await User.findById(id);
+
+    if (!userExist){
+      return res.status(404).json({ message: "User not found." });
+    }
+    const updatedData = await User.findByIdAndUpdate(id, req.body,{new:true})
+    res.status(200).json(updatedData);
+
+  }catch(error){
+    res.status(500).jason({errorMessage: error.message});
+  }
+};
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 
-import {create, getAllUsers, getUserByID} from "../controller/userController.js";
+import {create, getAllUsers, getUserByID, update} from "../controller/userController.js";
 
 const route = express.Router();
 
 route.post("/user", create);
-route.get("/users",getAllUsers)
-route.get("/user/:id",getUserByID)
+route.get("/users",getAllUsers);
+route.get("/user/:id",getUserByID);
+route.put("/update/user/:id",update);
 
 export default route;
