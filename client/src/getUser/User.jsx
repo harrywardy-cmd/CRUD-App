0187fdb7c0d060da38a563754b3a70d2aed073c9
harrywardy-cmd <harrywardy303@gmail.com>
2025-09-18 22:25:18 +1000

    adding css to react front end

--- client/src/getUser/User.jsx
+++ client/src/getUser/User.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import "./User.css"
 const User = () => {
     return(
-        <div>User Table</div>
+        <div className="userTable">
+            <button type="button" class="btn btn-primary">Add User</button>
+        <table className="table table-bordered">
+                <thead>
+                <tr>
+                    <th scope="col">S.No.</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Address</th>
+                    <th scope="col">Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3 </td>
+                        <td>4</td>
+                        <td>Update | Delete</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     )
 }
 
