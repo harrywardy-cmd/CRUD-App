bb16e097c76c35fdd615f48d225b4c00824ed29b
harrywardy-cmd <harrywardy303@gmail.com>
2025-10-29 00:49:13 +1100

    add hot toast bpm

--- client/src/addUser/AddUser.jsx
+++ client/src/addUser/AddUser.jsx
@@ -1,22 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./adduser.css";
-import {Link} from "react-router-dom";
+import {Link,useNavigate} from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
+
 
 
 const AddUser = () => {
+    const users = {
+      name:"",
+      email:"",
+      address: "",
+    };
+    const [user, setUser] = useState(users);
+    const navigate = useNavigate();
+
+    const inputHandler = (e) =>{
+      const {name, value} = e.target;
+      console.log(name, value);
+
+      setUser({...user, [name]:value});
+    };
+
+    const submitForm = async(e)=>{
+      e.preventDefault();
+       await axios.post("http://localhost:8000/api/user",user)
+       .then((response)=>{
+          //console.log("User created successfully.");
+          toast.success(response.data.message,{position: "top-right"});
+          navigate("/")
+       })
+       .catch((error)=>{
+        console.log(error)
+       })
+    }
   return (
     
-    <div className="addUser">
+    <div className="addUser"> 
       <Link to="/" type="button" class="btn btn-secondary">
         <i class="fa-solid fa-backward"></i> Back
       </Link>
       <h3>Add New User</h3>
-      <form className="addUserForm">
+      <form className="addUserForm" onSubmit={submitForm}>
         <div className="inputGroup">
           <label htmlFor="name">Name:</label>
           <input
             type="text"
             id="name"
+            onChange={inputHandler}
             name="name"
             autoComplete="off"
             placeholder="Enter your Name"
@@ -27,6 +58,7 @@ const AddUser = () => {
           <input
             type="email"
             id="email"
+            onChange={inputHandler}
             name="email"
             autoComplete="off"
             placeholder="Enter your Email"
@@ -37,6 +69,7 @@ const AddUser = () => {
           <input
             type="text"
             id="address"
+            onChange={inputHandler}
             name="address"
             autoComplete="off"
             placeholder="Enter your Address"
